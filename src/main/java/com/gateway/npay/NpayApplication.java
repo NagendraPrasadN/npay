530e36a0c8529d8361d73b807dcd2a20fbb548ef
NagendraPrasadN <nagendra.prasad@opsmx.io>
2026-01-19 17:48:19 +0530

    initial pay app

---
+++ src/main/java/com/gateway/npay/NpayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.npay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gateway/npay/NpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.npay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
